a874ecb40cdd0f610e1d8c0b735f240ecc8a76b5
Francesc116 <info@rmsonce.com>
2023-11-28 08:53:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,30 +5,10 @@ controllTD.addEventListener('input', (event) => {
   ws.send(JSON.stringify({ 'slider1': controllTD.value / 100 }));
 }, false);
 
-let controlledByTD = document.querySelector('.controlledByTD');
-
 ws.addEventListener('open', (event) => {
   console.log('Socket connection open');
   // alert('Successfully connected to socket server 🎉');
   ws.send('pong');
-});
-
-ws.addEventListener('message', (message) => {
-  if (message && message.data) {
-    if (message.data === "ping") {
-      console.log("got ping");
-      ws.send("pong");
-      return;
-    }
-    let data = JSON.parse(message.data);
-    if (data) {
-      if ("slider1" in data) {
-        controlledByTD.value = data["slider1"] * 100;
-      }
-      console.log("got data", data);
-    }
-  }
-  console.log("message", message)
 });
 
 ws.addEventListener('error', (error) => {
