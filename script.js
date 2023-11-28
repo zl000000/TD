839850856b095b504a46d1812d9ae64a6c27232c
zl000000 <152258636+zl000000@users.noreply.github.com>
2023-11-28 17:52:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("wss://websocket-p75n.onrender.com/:443");
+let ws = new WebSocket("wss://zl000000.onrender.com");
 
 let controllTD = document.querySelector('.controllTD') ;
 controllTD.addEventListener('input', (event) => {
